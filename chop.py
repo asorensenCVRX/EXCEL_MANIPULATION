060e4d1756d615c8eb9bd177e2db2dc669f1ff3e
asorensen <asorensen@cvrx.com>
2025-04-21 10:44:35 -0600

    add email function

--- chop.py
+++ chop.py
@@ -1,9 +1,13 @@
 from openpyxl.reader.excel import load_workbook
 import os
 from openpyxl.worksheet.table import Table, TableStyleInfo
+import win32com.client
+import html
+import os
+from pprint import pprint
 
-source = r"C:\Users\asorensen\OneDrive - CVRx Inc\2025_COMP_OPS\Clinical Targets\All 2025 Targets - Copy.xlsx"
-target_dir = r"C:\Users\asorensen\OneDrive - CVRx Inc\2025_COMP_OPS\Clinical Targets"
+source = r"C:\Users\asorensen\OneDrive - CVRx Inc\2025_COMP_OPS\DocuSign_Process\STATUS REPORTS\Envelope Recipient Report 4.21.25.xlsx"
+target_dir = r"C:\Users\asorensen\PycharmProjects\EXCEL_MANIPULATION\files"
 
 
 def create_files(source_file, tab, column, target_directory):
@@ -18,9 +22,12 @@ def create_files(source_file, tab, column, target_directory):
     col_index = None
 
     # add all distinct column values to the criteria variable and save the column letter to a variable
+    # iterate over each column in the first row (header row)
     for col in sheet.iter_cols(min_row=1, max_row=1):
+        # check if the column header matches the name provided as the column variable
         if col[0].value == column:
             col_index = col[0].col_idx
+            # iterate over all cells in the column, skipping the header
             for cell in sheet[col[0].column_letter][1:]:
                 if cell.value not in criteria:
                     criteria.append(cell.value)
@@ -32,6 +39,8 @@ def create_files(source_file, tab, column, target_directory):
     for item in criteria:
         workbook = load_workbook(source_file)
         sheet = workbook[tab]
+
+        # UPDATE THIS VARIABLE TO CHANGE THE NAME OF THE SAVED FILE
         save_path = os.path.join(target_directory, f"{item}.xlsx")
 
         # remove table formatting
@@ -58,4 +67,37 @@ def create_files(source_file, tab, column, target_directory):
         # save
         workbook.save(save_path)
 
-create_files(source_file=source, tab="Data", column='REGION', target_directory=target_dir)
+
+def send_emails(files_directory, email_column, email_template):
+    files_and_recipients = {}
+    for filename in os.listdir(files_directory):
+        filepath = os.path.join(files_directory, filename)
+        workbook = load_workbook(filepath)
+        sheet = workbook.active
+        emails = []
+        for col in sheet.iter_cols(min_row=1, max_row=1):
+            if col[0].value == email_column:
+                for cell in sheet[col[0].column_letter][1:]:
+                    if cell.value not in emails:
+                        emails.append(cell.value)
+                    else:
+                        continue
+                break
+        files_and_recipients[filepath] = emails
+    # pprint(files_and_recipients)
+
+    for key, value in files_and_recipients.items():
+        outlook = win32com.client.Dispatch('Outlook.Application')
+        mail = outlook.CreateItemFromTemplate(email_template)
+        # mail = outlook.CreateItem(0)
+        # mail.Body = """testing testing testing"""
+        mail.Subject = f'Unsigned DocuSigns'
+        mail.To = '; '.join(value)
+        mail.CC = 'jmoore@cvrx.com'
+        mail.Attachments.Add(fr'{key}')
+
+        mail.Send()
+
+
+create_files(source_file=source, tab="Sheet1", column='Region', target_directory=target_dir)
+# send_emails(files_directory=target_dir, email_column="ASD Email", email_template=r"C:\Users\asorensen\PycharmProjects\EXCEL_MANIPULATION\eml.oft")
